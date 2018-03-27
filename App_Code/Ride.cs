4105b300da20a4c4612e657e555a604d2cff1548
MaDo1989 <33927366+MaDo1989@users.noreply.github.com>
2018-03-27 19:29:58 +0300

    Added info to GetRidesForNotify - for trial

--- App_Code/Ride.cs
+++ App_Code/Ride.cs
@@ -18,13 +18,17 @@ public class Ride
 
     public int Id { get; set; }
 
+    public Destination Origin { get; set; }
+
+    public Destination Destination { get; set; }
+
     public List<RidePat> RidePats { get; set; }
 
     public List<Volunteer> Drivers { get; set; }
 
     public string Status { get; set; }
 
-    public List<Ride> GetRidesForNotify()
+    public List<Ride> GetRidesForNotifyfull()
     {
         string query = "select * from RideViewForNotify where statusRide='שובץ נהג' or statusRide='מלאה'";
         DbService db = new DbService();
@@ -46,7 +50,7 @@ public class Ride
                     RideExists = true;
                     foreach (RidePat ridepat in ride.RidePats)
                     {
-                        if (ridepat.RidePatNum==int.Parse(dr["ridePatNum"].ToString()))
+                        if (ridepat.RidePatNum == int.Parse(dr["ridePatNum"].ToString()))
                         {
                             RidePatexists = true;
                             break;
@@ -79,9 +83,9 @@ public class Ride
                     //rp.Shift = dr["RidePatShift"].ToString();
                     rp.Date = Convert.ToDateTime(dr["dateRide"].ToString());
                     rp.Pat.CellPhone = dr["PatientCell"].ToString();
-                    
+
                     ride.RidePats.Add(rp);
-                    
+
                 }
             }
             if (RideExists) continue;
@@ -115,6 +119,170 @@ public class Ride
         }
 
         return rides;
+    }
+
+    public List<Ride> GetRidesForNotify()
+    {
+        List<Ride> lr = new List<Ride>();
+
+        //Ride 1
+        Ride r1 = new Ride();
+        r1.Origin = new Destination();
+        r1.Origin.Name = "ארז";
+        r1.Destination = new Destination();
+        r1.Destination.Name = "הלל יפה";
+        r1.RidePats = new List<RidePat>();
+        r1.Drivers = new List<Volunteer>();
+        
+        
+
+        //Volunteers-Drivers
+        Volunteer v = new Volunteer();
+        v.DisplayName = "אסף סלוק";
+        v.CellPhone = "052-1111111";
+        v.DriverType = "ראשי";
+        r1.Drivers.Add(v);
+        Volunteer v2 = new Volunteer();
+        v2.DisplayName = "מתן דורון";
+        v2.CellPhone = "052-2222222";
+        v2.DriverType = "גיבוי";
+        r1.Drivers.Add(v2);
+
+       
+        r1.RidePats = new List<RidePat>();
+
+        //RidePat
+        RidePat rp = new RidePat();
+        rp.Date = new DateTime(2018,4,1,6,0,0);
+        rp.StartPlace = new Destination();
+        rp.StartPlace.Name = "ארז";
+        rp.Target = new Destination();
+        rp.Target.Name = "הלל יפה";
+        rp.Coordinator = new Volunteer();
+        rp.Coordinator.DisplayName = "בני בורנפלד";
+        rp.Coordinator.CellPhone = "050-0000000";
+
+        //Patient
+        rp.Pat = new Patient();
+        rp.Pat.DisplayName = "וואליד באדיר";
+        rp.Pat.CellPhone = "050-9999999";
+        rp.Pat.Id = 1;
+
+        //Escort
+        rp.Pat.EscortedList = new List<Escorted>();
+        Escorted e = new Escorted();
+        e.DisplayName = "מוחמד גאדיר";
+        e.CellPhone = "050-8888888";
+
+        rp.Pat.EscortedList.Add(e);
+
+        //Equipment
+        rp.Pat.Equipment = new List<string>();
+        rp.Pat.Equipment.Add("כסא גלגלים");
+        rp.Pat.Equipment.Add("קביים");
+
+        r1.RidePats.Add(rp);
+        lr.Add(r1);
+
+        //Ride 2
+        Ride r2 = new Ride();
+        r2.Origin = new Destination();
+        r2.Origin.Name = "ג'למה";
+        r2.Destination = new Destination();
+        r2.Destination.Name = "רמבם";
+        r2.RidePats = new List<RidePat>();
+        r2.Drivers = new List<Volunteer>();
+
+        //Volunteers-Drivers
+        Volunteer vv = new Volunteer();
+        vv.DisplayName = "מסי";
+        vv.CellPhone = "052-3333333";
+        vv.DriverType = "ראשי";
+        r2.Drivers.Add(vv);
+        Volunteer vv2 = new Volunteer();
+        vv2.DisplayName = "רונאלדו";
+        vv2.CellPhone = "052-4444444";
+        vv2.DriverType = "גיבוי";
+        r2.Drivers.Add(vv2);
+
+
+        r2.RidePats = new List<RidePat>();
+        
+        //RidePat 1
+        RidePat rp1 = new RidePat();
+        rp1.Date = new DateTime(2018, 4, 5, 11, 0, 0);
+        rp1.StartPlace = new Destination();
+        rp1.StartPlace.Name = "ג'למה";
+        rp1.Target = new Destination();
+        rp1.Target.Name = "רמבם";
+        rp1.Coordinator = new Volunteer();
+        rp1.Coordinator.DisplayName = "בני בורנפלד";
+        rp1.Coordinator.CellPhone = "050-0000000";
+
+        //Patient
+        rp1.Pat = new Patient();
+        rp1.Pat.DisplayName = "אדיר מילר";
+        rp1.Pat.CellPhone = "050-7777777";
+        rp1.Pat.Id = 2;
+
+        //Escort
+        rp1.Pat.EscortedList = new List<Escorted>();
+        Escorted ee = new Escorted();
+        ee.DisplayName = "שחר חסון";
+        ee.CellPhone = "050-6666666";
+
+        rp1.Pat.EscortedList = new List<Escorted>();
+        Escorted ee2 = new Escorted();
+        ee2.DisplayName = "קובי מימון";
+        ee2.CellPhone = "050-5555555";
+
+        rp1.Pat.EscortedList.Add(ee);
+        rp1.Pat.EscortedList.Add(ee2);
+
+        //Equipment
+        rp1.Pat.Equipment = new List<string>();
+        rp1.Pat.Equipment.Add("בוסטר");
+
+        r2.RidePats.Add(rp1);
+
+        //RidePat 2
+        RidePat rp2 = new RidePat();
+        rp2.Date = new DateTime(2018, 4, 5, 11, 0, 0);
+        rp2.StartPlace = new Destination();
+        rp2.StartPlace.Name = "ג'למה";
+        rp2.Target = new Destination();
+        rp2.Target.Name = "רמבם";
+        rp2.Coordinator = new Volunteer();
+        rp2.Coordinator.DisplayName = "בני בורנפלד";
+        rp2.Coordinator.CellPhone = "050-0000000";
+
+        //Patient
+        rp2.Pat = new Patient();
+        rp2.Pat.DisplayName = "שלמה ארצי";
+        rp2.Pat.CellPhone = "050-1234567";
+        rp2.Pat.Id = 3;
+
+        //Escort
+        rp2.Pat.EscortedList = new List<Escorted>();
+        Escorted ee3 = new Escorted();
+        ee3.DisplayName = "אבי סינגולדה";
+        ee3.CellPhone = "050-0987654";
+
+        rp2.Pat.EscortedList = new List<Escorted>();
+        Escorted ee4 = new Escorted();
+        ee4.DisplayName = "מאיר ישראל";
+        ee4.CellPhone = "050-6758493";
+
+        rp2.Pat.EscortedList.Add(ee3);
+        rp2.Pat.EscortedList.Add(ee4);
+        
+
+        
+        r2.RidePats.Add(rp2);
+
+        lr.Add(r2);
+
+        return lr;
     }
 
     public List<Ride> GetMyRides(int volunteerId)
