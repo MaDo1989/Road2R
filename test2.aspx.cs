ac61060dbe6b793099a460dc8d3c1412db7c1d6f
MaDo1989 <33927366+MaDo1989@users.noreply.github.com>
2018-06-28 10:17:18 +0300

    Added logger

---
+++ test2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using log4net;
+
+public partial class test2 : System.Web.UI.Page
+{
+    private static readonly ILog Log =
+                LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+
+            Log.Error("this is a new error message");
+
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+    }
+}
