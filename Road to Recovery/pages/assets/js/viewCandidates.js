e69e5c4dd207698ff67aacadc971e0dbfbc3f07e
Yogev Strauber <57799514+Yogev-Strauber@users.noreply.github.com>
2020-12-22 00:55:34 +0200

    viewCandidate module work in progress, page is in it first developing steps..

--- Road to Recovery/pages/assets/js/viewCandidates.js
+++ Road to Recovery/pages/assets/js/viewCandidates.js
@@ -1,5 +1,8 @@
 ﻿checkCookie();
-let ridepat;
+let { convertDBDate2FrontEndDate } = GENERAL.USEFULL_FUNCTIONS;
+let { getRidePatNum4_viewCandidate } = GENERAL.RIDEPAT;
+let { ajaxCall } = GENERAL.FETCH_DATA;
+let thisRidePat;
 
 const wiringDataTables = () => {
     //manage button clicks on tables
@@ -13,7 +16,6 @@ const wiringDataTables = () => {
 }
 
 $(document).ready(() => {
-
     fetchData4ThisRidepat();
 
     if (!JSON.parse(localStorage.getItem("isProductionDatabase"))) {
@@ -23,7 +25,7 @@ $(document).ready(() => {
     $('[data-toggle="tooltip"]').tooltip({
         container: 'body'
     })
-    if (window.location.hostname.toString() == 'localhost' || window.location.pathname.indexOf('test') != -1) {
+    if (window.location.hostname.toString() == 'localhost' || window.location.pathname.toLowerCase().indexOf('test') != -1) {
         $("#na").css("background-color", "#ffde89");
     }
     if (window.location.href.indexOf('http://40.117.122.242/Road%20to%20Recovery/') != -1) {
@@ -42,49 +44,12 @@ $(document).ready(() => {
     includeHTML();//with out this there is no side bar!
 
 
-    jQuery.extend(jQuery.fn.dataTableExt.oSort, {
-        "customdatesort-pre": function (a) {
-            var r, x;
-            if (a === null || a === "") {
-                r = false;
-            } else {
-                x = a.split("/");
-                r = +new Date(+x[2], +x[1] - 1, +x[0]);
-            }
-            //console.log("[PRECALC] " + a + " becomes " + r);
-            return r;
-        },
-        "customdatesort-asc": function (a, b) {
-            if (a === false && b === false) {
-                return 0;
-            } else if (a === false) {
-                return 1;
-            } else if (b === false) {
-                return -1;
-            } else {
-                return a - b;
-            }
-        },
-        "customdatesort-desc": function (a, b) {
-            if (a === false && b === false) {
-                return 0;
-            } else if (a === false) {
-                return 1;
-            } else if (b === false) {
-                return -1;
-            } else {
-                return b - a;
-            }
-        }
-    });
-
-
 
     /*
-                 ============================
-                 || ↓DATATABLES PROPERTIES↓||
-                 ============================
-    */
+             ============================
+             || ↓DATATABLES PROPERTIES↓||
+             ============================
+*/
     candidatesTable = $('#datatable-candidates').DataTable(
         {
             "stateDuration": 60 * 60,
@@ -139,18 +104,21 @@ $(document).ready(() => {
 
 const fetchData4ThisRidepat = () => {
 
-    let ridePatNum = JSON.parse(GENERAL.RIDEPAT.getRidePatNum4_viewCandidate());
-    GENERAL.FETCH_DATA.ajaxCall(
-                                'GetRidePat',
-                                JSON.stringify({ ridePatNum }),
-                                fetchData4ThisRidepat_SCB,
-                                fetchData4ThisRidepat_ECB
+    let ridePatNum = JSON.parse(getRidePatNum4_viewCandidate());
+    ajaxCall(
+        'GetRidePat',
+        JSON.stringify({ ridePatNum }),
+        fetchData4ThisRidepat_SCB,
+        fetchData4ThisRidepat_ECB
     );
 }
 
 const fetchData4ThisRidepat_SCB = (data) => {
-    ridepat = JSON.parse(data.d);
-    //catch this id RideCandidates_ph
+    thisRidePat = JSON.parse(data.d); //think if this variable should be global or local & pass throw this function below
+
+    console.log(thisRidePat);
+
+    useRidePatData();
 }
 
 const fetchData4ThisRidepat_ECB = (data) => {
@@ -160,18 +128,35 @@ const fetchData4ThisRidepat_ECB = (data) => {
 }
 
 
+const useRidePatData = () => {
+    
+    let date = convertDBDate2FrontEndDate(thisRidePat.Date);
+    let hh = date.getHours(); 
+    let mm = date.getMinutes();
+    //YOGEV STOPED HERE IN 22.12.2020 1:00 AM 
+    let rideCandidatesHeadLine = `מ לשיבא, היום ב6:15 בבוקר`;
+    rideCandidatesHeadLine += ` ${thisRidePat.Origin.Name} `;
+    rideCandidatesHeadLine += `ל`;
+    rideCandidatesHeadLine += ` ${thisRidePat.Destination.Name} `;
+    rideCandidatesHeadLine += ``;
+    rideCandidatesHeadLine += ``;
+    rideCandidatesHeadLine += ``;
 
-function refreshTable() {
-    //???
-
+    document.getElementById('RideCandidates_ph').innerHTML = rideCandidatesHeadLine;
 }
 
 
+//function refreshTable() {
+//    //???
+
+//}
 
 
-function manipulateTables() {
 
-    tableMorning = document.getElementById("datatable-morning");
-    tableAfternoon = document.getElementById("datatable-afternoon");
 
-}
+//function manipulateTables() {
+
+//    tableMorning = document.getElementById("datatable-morning");
+//    tableAfternoon = document.getElementById("datatable-afternoon");
+
+//}
