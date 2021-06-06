bd58a6a6cafe1391e0531f948d2b48752912956e
Yogev Strauber <yogev.strauber@gmail.com>
2021-06-06 11:56:38 +0300

    finish configurations for signalR

---
+++ App_Code/Startup1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Startup1))]
+
+public class Startup1
+{
+    public void Configuration(IAppBuilder app)
+    {
+        app.MapSignalR();
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+    }
+}
