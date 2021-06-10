0c107ce04fe35df188be914f556afaa14f0a2ae4
Yogev Strauber <yogev.strauber@gmail.com>
2021-06-10 12:22:25 +0300

    signalR init

---
+++ App_Code/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class ChatHub : Hub
+{
+    public void Send(string name, string message)
+    {
+        Clients.All.broadcastMessage(name, message);
+
+    }
+}
---
+++ App_Code/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        app.MapSignalR();
+    }
+}
