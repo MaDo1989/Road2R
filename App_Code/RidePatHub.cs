88e8a8261160c226b2fe1b287678d694672cbc29
Yogev Strauber <yogev.strauber@gmail.com>
2021-07-03 23:47:45 +0300

    Update RidePatHub.cs
    1. add Broad-Cast driver Removal method to RidePatHub

--- App_Code/RidePatHub.cs
+++ App_Code/RidePatHub.cs
@@ -4,7 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Web;
-
+/*
+     ===============================
+     !!! PAY VERY GOOD ATTENTION !!!
+     !!! CHANGES HERE MUST TAKE  !!!
+     !!!   PLACE ON RELEVANT     !!!
+     !!! PLACES ON CLIENT SIDE   !!!
+     ===============================
+*/
 public class RidePatHub : Hub
 {
     public void BroadCast_driverAssigning(RidePat rp)
@@ -12,4 +19,8 @@ public class RidePatHub : Hub
         Clients.All.driverHasAssigned2RidePat(rp);
     }
 
+    public void BroadCast_driverRemoval(RidePat rp)
+    {
+        Clients.All.driverHasRemovedFromRidePat(rp);
+    }
 }
