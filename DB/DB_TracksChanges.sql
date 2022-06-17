a6f2fa8558cdaa03cde87681f2db6981d80d34b7
Yogev Strauber <yogev.strauber@gmail.com>
2022-06-17 18:16:39 +0300

    Update DB_TracksChanges.sql
    Add procedure to assign driver which will be taking care of all relevant logics when it comes to assign driver
    not finished yet but it is much faster now

--- DB/DB_TracksChanges.sql
+++ DB/DB_TracksChanges.sql
@@ -13,7 +13,7 @@
 -- ALTER DATE: 29/04/2022
 -- ALTER REASON: Add NoOfDocumentedRides
 -- =============================================
-	ALTER PROCEDURE [dbo].[spGetCandidatesForRidePat]
+CREATE OR ALTER PROCEDURE [dbo].[spGetCandidatesForRidePat]
 	(
 		@RidePatNum INT,
 		@NumOfDaysToThePast INT,					@NUmOfDaysToTheFuture INT,
@@ -175,7 +175,7 @@ GO
 -- ALTER DATE: 29/04/2022
 -- ALTER REASON: Add NoOfDocumentedRides
 -- =============================================
-	ALTER PROCEDURE [dbo].[spGetNoobsCandidatesForRidePat]
+CREATE OR ALTER PROCEDURE [dbo].[spGetNoobsCandidatesForRidePat]
 	(
 		@RidePatNum INT,
 		@NumOfDaysToThePast INT,					@NUmOfDaysToTheFuture INT,
@@ -365,7 +365,7 @@ GO
 -- ALTER DATE:   ---
 -- ALTER REASON: ---
 -- =============================================
-CREATE PROCEDURE spRide_UpdateDriver
+CREATE OR ALTER PROCEDURE spRide_UpdateDriver
 (
 		@RideNum INT,
 		@NewDriverId INT
@@ -378,3 +378,72 @@ BEGIN
 	WHERE RideNum = @RideNum 
 END
 GO
+
+-- =============================================
+-- Author:      Yogev Strauber
+-- Description: Assign Driver To Ride And Wire Ride To RidePat
+-- ALTER DATE:   ---
+-- ALTER REASON: ---
+-- =============================================
+CREATE OR ALTER PROCEDURE spRideAndRidePat_AssignDriver
+(
+	@ridePatId int,
+	@rideId int,
+	@driverId int,
+	@assignedFromAppId int
+
+)
+AS
+BEGIN
+
+    SET NOCOUNT ON
+	DECLARE 
+		@Success NVARCHAR(75)						= N'הנסיעה עודכנה בהצלחה',
+		@AllReadyAssigned NVARCHAR(75)				= N'הנסיעה אליה נרשמת כבר מלאה',
+		@NotExistsRidePat NVARCHAR(75)				= N'נסיעה זו בוטלה, תודה על הרצון לעזור',
+		@DriverIsNotActiveNorDriving NVARCHAR(75)	= N'הנהג אינו פעיל/ אינו נוהג או שניהם',
+		@Origin NVARCHAR(255)						= (SELECT Origin FROM RidePat WHERE RidePatNum=@ridePatId),
+		@Destination NVARCHAR(255)					= (SELECT Destination FROM RidePat WHERE RidePatNum=@ridePatId),
+		@Date datetime								= (SELECT PickupTime FROM RidePat WHERE RidePatNum=@ridePatId),
+		@CreatedRideId INT							= NULL
+
+
+	IF NOT EXISTS (SELECT 1	FROM RidePat WHERE RidePatNum=@ridePatId)
+	BEGIN
+		SELECT 1 AS IsError, @NotExistsRidePat AS Message
+		RETURN;
+	END
+
+	IF (SELECT MainDriver FROM RPView WHERE RidePatNum=@ridePatId) IS NOT NULL 
+	BEGIN
+		SELECT 1 AS IsError, @AllReadyAssigned AS Message
+		RETURN;
+	END
+
+	IF NOT EXISTS (SELECT 1 FROM Volunteer WHERE Id=@driverId AND IsActive=1 AND isDriving=1)
+		BEGIN
+		SELECT 1 AS IsError, @DriverIsNotActiveNorDriving AS Message
+		RETURN;
+		END
+
+	IF EXISTS (SELECT 1 FROM Ride where RideNum=@rideId)
+		BEGIN
+				UPDATE Ride SET MainDriver=@driverId, AssignedFromAppId = @assignedFromAppId WHERE RideNum=@rideId
+				SELECT 0 AS IsError, @Success AS Message, @rideId AS RideId
+		END
+	ELSE
+		BEGIN
+			SET DATEFORMAT dmy;
+
+			INSERT INTO Ride (Origin,Destination,Date,MainDriver, AssignedFromAppId) 
+			VALUES (@origin,@Destination,@date,@driverId, @assignedFromAppId) 
+
+			SET @CreatedRideId = (SELECT SCOPE_IDENTITY());
+
+			UPDATE RidePat SET RideId=@CreatedRideId WHERE RidePatNum=@ridePatId
+			
+			SELECT 0 AS IsError, @Success AS Message, @CreatedRideId AS RideId
+		END
+END
+GO
+
