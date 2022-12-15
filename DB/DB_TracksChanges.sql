c85b807302124950830ff9f5eae1d8681a6d321a
Yogev Strauber <yogev.strauber@gmail.com>
2022-12-15 23:36:42 +0200

    Update DB_TracksChanges.sql
    merge both Yogi's and Benny's SQL script

--- DB/DB_TracksChanges.sql
+++ DB/DB_TracksChanges.sql
@@ -27,7 +27,7 @@ update City set ismain = 1 where CityName = N'תל אביב - יפו'
 
 update City set mainCity = CityName where isMain = 1
 
-
+GO
 -- =============================================
 -- Author:      Benny Bornfeld
 -- Create Date: 09/12/2022
@@ -42,13 +42,6 @@ BEGIN
 END
 GO
 
-
--- =======================================================
--- Create Stored Procedure Template for Azure SQL Database
--- =======================================================
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
 -- Author:      <Benny>
@@ -72,14 +65,6 @@ BEGIN
 END
 GO
 
-/****** Object:  StoredProcedure [dbo].[spCity_GetAllCitiesWithLocations]    Script Date: 12/15/2022 5:02:07 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
 -- =============================================
 -- Author:      Benny Bornfeld
 -- Create Date: 15/12/2022
@@ -94,13 +79,6 @@ BEGIN
 END
 GO
 
--- =======================================================
--- Create Stored Procedure Template for Azure SQL Database
--- =======================================================
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 -- =============================================
 -- Author:      <Benny B>
 -- Create Date: <15-12-2022>
@@ -120,3 +98,170 @@ select cityname, mainCity from city where cityname in (select citycityname from 
 END
 GO
 
+-- =============================================
+-- Author:      Yogev Strauber
+-- Create Date: 03/12/2022 @night
+-- Description: Active Or Deactivate Volunteer Base on isActive Parameter
+-- Returns IsSuccesfulOperation bit, and optional VolunteerWithFutureRidesIncludedToday
+-- when try to deactivate volunteer
+-- =============================================
+CREATE OR ALTER   PROCEDURE [dbo].[spVolunteer_ToggleActiveness]
+(
+   @displayName NVARCHAR(255),
+   @isActive BIT
+		)
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+	DECLARE @volunteerId INT = (SELECT Id from volunteer where Displayname=@displayName)
+
+	IF(@isActive) = 0
+	BEGIN
+		IF(
+			SELECT COUNT(*) FROM RIDE 
+			WHERE MAINDRIVER=14430
+			AND  CONVERT(VARCHAR, GETDATE(), 110) <= CONVERT(VARCHAR, Date, 110) 
+		   ) = 0
+			BEGIN
+				UPDATE Volunteer 
+				SET IsActive=@isActive, 
+				lastModified=DATEADD(hour, 2, SYSDATETIME())
+				WHERE Id=@volunteerId
+			SELECT 
+				1 AS IsSuccesfulOperation,
+				0 AS VolunteerWithFutureRidesIncludedToday
+			END
+		ELSE
+			BEGIN
+			SELECT
+				0 AS IsSuccesfulOperation,
+				1 AS VolunteerWithFutureRidesIncludedToday
+			END
+	END
+	ELSE
+	BEGIN 
+			UPDATE Volunteer 
+			SET IsActive=@isActive, 
+			lastModified=DATEADD(hour, 2, SYSDATETIME())
+			WHERE Id=@volunteerId
+
+			SELECT
+				1 AS IsSuccesfullOperation
+END
+END
+GO
+
+-- =============================================
+-- Author:      Yogev Strauber
+-- Create Date: 14/12/2022 @night
+-- Description: Active Or Deactivate Volunteer's isDriving
+-- Returns IsSuccesfullOperation bit, and optional VolunteerWithFutureRidesIncludedToday
+-- when try to deactivate volunteer
+-- =============================================
+CREATE    PROCEDURE [dbo].[spVolunteer_ToggleIsDrive]
+(
+   @displayName NVARCHAR(255),
+   @isDriving BIT
+		)
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+	DECLARE @volunteerId INT = (SELECT Id from volunteer where Displayname=@displayName)
+
+	IF(@isDriving) = 0
+	BEGIN
+		IF(
+			SELECT COUNT(*) FROM RIDE 
+			WHERE MAINDRIVER=@volunteerId
+			AND  CONVERT(VARCHAR, GETDATE(), 110) <= CONVERT(VARCHAR, Date, 110) 
+		   ) = 0
+			BEGIN
+				UPDATE Volunteer 
+				SET IsActive=@isDriving, 
+				lastModified=DATEADD(hour, 2, SYSDATETIME())
+				WHERE Id=@volunteerId
+			SELECT 
+				1 AS IsSuccesfullOperation,
+				0 AS VolunteerWithFutureRidesIncludedToday
+			END
+		ELSE
+			BEGIN
+			SELECT
+				0 AS IsSuccesfullOperation,
+				1 AS VolunteerWithFutureRidesIncludedToday
+			END
+	END
+	ELSE
+	BEGIN 
+			UPDATE Volunteer 
+			SET isDriving=@isDriving, 
+			lastModified=DATEADD(hour, 2, SYSDATETIME())
+			WHERE Id=@volunteerId
+
+			SELECT
+				1 AS IsSuccesfullOperation
+
+END
+END
+GO
+
+-- Remove calculation of most common regional path
+ALTER procedure [dbo].[spVolunteerTypeView_GetVolunteersList]
+
+@IsActive bit
+as
+begin
+select r.MainDriver, r.Origin, r.Destination into #tempNotDeletedOnly from  ridepat rp
+inner join ride r
+on r.RideNum=rp.RideId
+
+if (@IsActive = 0)
+	begin
+				select *, (select count(*)
+					from ridepat rp inner join ride r
+					on rp.rideid=r.ridenum
+					where r.maindriver = vtv.Id
+					and pickuptime between DATEADD(Month, -2, GETDATE()) and  GETDATE()) as NumOfRides_last2Months
+					,
+					(
+				select origin + '-'+destination from
+													(
+														select top 1 maindriver, origin, destination, count(*) as numberOfTimesDrove FROM #tempNotDeletedOnly t
+														where t.MainDriver = id
+														group by maindriver, origin, destination
+														order by numberOfTimesDrove desc
+														) t
+					) mostCommonPath
+		from VolunteerTypeView vtv
+		where IsActive = @IsActive or IsActive = 1
+		order by firstNameH
+
+	end
+else
+	begin
+	select *, (select count(*)
+					from ridepat rp inner join ride r
+					on rp.rideid=r.ridenum
+					where r.maindriver = vtv.Id
+					and pickuptime between DATEADD(Month, -2, GETDATE()) and  GETDATE()) as NumOfRides_last2Months
+					,
+					(
+				select origin + '-'+destination from
+													(
+														select top 1 maindriver, origin, destination, count(*) as numberOfTimesDrove FROM #tempNotDeletedOnly t
+														where t.MainDriver = id
+														group by maindriver, origin, destination
+														order by numberOfTimesDrove desc
+														) t
+					) mostCommonPath
+		from VolunteerTypeView vtv
+		where IsActive = @IsActive
+		order by firstNameH
+	end
+
+	drop table #tempNotDeletedOnly 
+
+end
+GO
