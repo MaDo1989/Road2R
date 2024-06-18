01efd347c4c77ca5e310b7e03f0057b7f12bd0f6
Gilad Meirson <gilad.meirson@gmail.com>
2024-06-18 19:16:00 +0300

    i

--- Road to Recovery/pages/lib/rideMessages.js
+++ Road to Recovery/pages/lib/rideMessages.js
@@ -84,7 +84,7 @@ function buildMessage(message) {
     let firstName = message.driver.split(" ")[0];
     //let txt = `שלום ${message.driver}` + sep;
     let txt = `,שלום ${firstName}` + sep;
-    console.log('message.totalPeople', message.totalPeople)
+    //console.log('message.totalPeople', message.totalPeople)
     txt += `הסעה מ${message.origin} ל${message.destination}` + sep;
     if (message.totalPeople === 1) txt += `סה"כ אדם אחד` + sep;
     else txt += `סה"כ ${message.totalPeople} אנשים` + sep;
